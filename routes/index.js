3c0323fed58ef1b687dd515d3cb829248e2a4872
Antoine <contact@cln.ac>
2020-04-14 10:59:28 +0200

    initial index page canvas

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const post = require('../models/post');
 
 router.get('/', (req, res) =>{
-	res.render('index.hbs', {post: posts});
+	post.find({}, (err, posts) => {
+		if (err) res.render('index.hbs', {msg: "cannot load posts"});
+		else if (!posts.length) res.render('index.hbs', {msg: "nothing to see here; yet"});
+		else res.render('index.hbs', {posts: posts.map(post => post.to_display_format())});
+	});
+});
+
+router.get('/:post', (req, res) =>{
+	res.send("wip");
 });
 
 module.exports = router;
